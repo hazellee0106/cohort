163a1e296b2b1299ddb4f326b886b417e33e95ad
hazellee0106 <mkt.hazellee@gmail.com>
2016-03-21 11:27:21 +0900

    cohort_size table created

--- cohort.sql
+++ cohort.sql
@@ -4,3 +4,10 @@ SELECT customer_id,
 		MIN(rental_date) first_time
 FROM rental
 GROUP BY 1;
+
+DROP TEMPORARY TABLE IF EXISTS cohort_size;
+CREATE TEMPORARY TABLE cohort_size
+SELECT LEFT(first_time,7) Month,
+		COUNT(*) Num
+FROM first_rental
+GROUP BY 1;
