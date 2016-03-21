cada86665e0444a9042004df8512269e60958d54
hazellee0106 <mkt.hazellee@gmail.com>
2016-03-21 11:52:50 +0900

    add cohort

--- cohort.sql
+++ cohort.sql
@@ -7,7 +7,18 @@ GROUP BY 1;
 
 DROP TEMPORARY TABLE IF EXISTS cohort_size;
 CREATE TEMPORARY TABLE cohort_size
-SELECT LEFT(first_time,7) Month,
+SELECT LEFT(first_time,7) month,
 		COUNT(*) Num
 FROM first_rental
 GROUP BY 1;
+
+SELECT r.*,
+	LEFT(fr.first_time,7) cohort,
+	p.amount
+FROM rental r,
+	first_rental fr, 
+	cohort_size cs,
+	payment p
+WHERE r.customer_id=fr.customer_id
+	AND LEFT(fr.first_time,7)=cs.month
+	AND r.rental_id=p.rental_id;
